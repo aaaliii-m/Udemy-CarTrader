04af89e26f8405defcda809aaee45a5c6272737f
aaaliii-m <aaaliii4u@yahoo.com>
2024-10-11 03:31:49 +0500

    added cars card in cars page

--- app.vue
+++ app.vue
@@ -2,25 +2,15 @@
   <div>
     <NuxtRouteAnnouncer />
     <!--    NAVBAR   -->
-    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+    <header
+      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
+    >
       <a class="text-3xl font-mono" href="">cartrader</a>
     </header>
     <!--    NAVBAR   -->
-    <!--    CARS PAGE-->
-    <div>
-      <div>
-        <!--    CARS SIDEBAR-->
-        <div>
-          <h3>Location</h3>
-          <h3 class="text-blue-400 capitalize">toronto</h3>
-          <div></div>
-        </div>
-        <!--    CARS SIDEBAR-->
-        <!--    CARS CARD-->
-        <!--    CARS CARD-->
-      </div>
-    </div>
-    <!--    CARS PAGE-->
-
   </div>
 </template>
+
+<script setup lang="ts">
+const myFunc = () => {};
+</script>
